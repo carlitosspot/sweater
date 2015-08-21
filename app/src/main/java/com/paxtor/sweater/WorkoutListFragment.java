70389a01ae46e3055edc2bcf416d16948e447578
carlitosspot <renojpa@gmail.com>
2015-08-21 00:15:21 -0400

    adds listfragment

---
+++ app/src/main/java/com/paxtor/sweater/WorkoutListFragment.java
@@ -0,0 +1,30 @@
+package com.paxtor.sweater;
+
+
+import android.os.Bundle;
+import android.app.ListFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A simple {@link ListFragment} subclass.
+ */
+public class WorkoutListFragment extends ListFragment {
+
+
+    public WorkoutListFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+
+}
