4a41d5840afe80ef75dace06121a03fd08802ba5
carlitosspot <renojpa@gmail.com>
2015-08-21 00:07:50 -0400

    updates detail fragment code

--- app/src/main/java/com/paxtor/sweater/WorkoutDetailFragment.java
+++ app/src/main/java/com/paxtor/sweater/WorkoutDetailFragment.java
@@ -6,13 +6,14 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class WorkoutDetailFragment extends Fragment {
 
+    private long workoutId;
 
     public WorkoutDetailFragment() {
         // Required empty public constructor
@@ -27,4 +28,19 @@ public class WorkoutDetailFragment extends Fragment {
     }
 
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        View view = getView();
+        if (view != null) {
+            TextView title = (TextView) view.findViewById(R.id.textTitle);
+            Workout workout = Workout.workouts[(int) workoutId]; title.setText(workout.getName());
+            TextView description = (TextView) view.findViewById(R.id.textDescription);
+            description.setText(workout.getDescription());
+        }
+    }
+
+    public void setWorkout(long id){
+        this.workoutId = id;
+    }
 }
